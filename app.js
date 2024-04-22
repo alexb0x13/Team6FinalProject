49b8449bf63ad459f8007426340241eb87839111
Alex <aleblakl@gmail.com>
2024-04-21 21:19:39 -0400

    first commit

--- app.js
+++ app.js
@@ -16,19 +16,17 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    
   ];
   res.render('index', { title: 'Home', blogs });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get('/teachers', (req, res) => {
+  res.render('teachers', { title: 'Teachers' });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
+app.get('/students', (req, res) => {
+  res.render('students', { title: 'Students' });
 });
 
 // 404 page
